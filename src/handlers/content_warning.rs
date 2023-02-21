77fd15cffc215a0b0542ef982f7f92b0024cb99f
municorn <municorn@musicaloft.com>
2023-02-20 22:14:03 -0600

    Add content_warning handler

---
+++ src/handlers/content_warning.rs
@@ -0,0 +1,124 @@
+use std::collections::HashSet;
+
+use async_trait::async_trait;
+use regex::Regex;
+use twitch_irc::message::{ReplyToMessage, ServerMessage};
+
+use super::{HandlerError, MessageHandler};
+use crate::bot::MuniBotTwitchIRCClient;
+
+pub struct ContentWarningHandler {
+    active_warning: Option<String>,
+    users_greeted: HashSet<String>,
+}
+
+impl ContentWarningHandler {
+    pub fn new() -> Self {
+        Self {
+            active_warning: None,
+            users_greeted: HashSet::new(),
+        }
+    }
+
+    async fn say_user_requested_warning(
+        &mut self,
+        client: &MuniBotTwitchIRCClient,
+        channel: &str,
+        addressee: &str,
+    ) -> Result<(), HandlerError> {
+        if let Some(warning) = &self.active_warning {
+            self.send_message(client, channel, &format!("hey {}, muni has issued a content/trigger warning for this stream: {}. please take care of yourself! it's okay to leave or mute if this content will make you uncomfortable. and you are loved no matter what!", addressee, warning)).await
+        } else {
+            self.send_message(client, channel, &format!("hey {}, there is no active content/trigger warning in effect. enjoy the stream ^-^ if current conversation is making you uncomfortable, you can use the 'subject change /srs' redeem to change the subject!", addressee)).await
+        }
+    }
+
+    async fn say_streamer_requested_warning(
+        &mut self,
+        client: &MuniBotTwitchIRCClient,
+        channel: &str,
+    ) -> Result<(), HandlerError> {
+        if let Some(warning) = &self.active_warning {
+            self.send_message(
+                client,
+                channel,
+                &format!(
+                    "hi muni! you have an active content/trigger warning in effect: \"{}\"",
+                    warning
+                ),
+            )
+            .await
+        } else {
+            self.send_message(
+                client,
+                channel,
+                "hi muni! you don't have a content/trigger warning issued right now.",
+            )
+            .await
+        }
+    }
+
+    async fn greet_user(
+        &mut self,
+        client: &MuniBotTwitchIRCClient,
+        channel: &str,
+        user_name: &str,
+    ) -> Result<(), HandlerError> {
+        if !self.users_greeted.contains(user_name) && let Some(warning) = &self.active_warning {
+            self.send_message(client, channel, &format!("welcome, {}! just so you know, muni has issued a content/trigger warning for this stream: {}. please take care of yourself! it's okay to leave or mute if this content will make you uncomfortable. and you are loved no matter what!", user_name, warning)).await?;
+            self.users_greeted.insert(user_name.to_string());
+        }
+
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl MessageHandler for ContentWarningHandler {
+    async fn handle_message(
+        &mut self,
+        client: &MuniBotTwitchIRCClient,
+        message: ServerMessage,
+    ) -> Result<bool, HandlerError> {
+        let handled = match message {
+            ServerMessage::Privmsg(m) => {
+                if let Some(content) = m
+                    .message_text
+                    .strip_prefix("!cw")
+                    .or(m.message_text.strip_prefix("!tw"))
+                    .map(|s| s.trim_start())
+                {
+                    if m.sender.login == m.channel_login {
+                        if content.trim().is_empty() {
+                            self.say_streamer_requested_warning(client, &m.channel_login)
+                                .await?;
+                        } else if content == "clear" || content == "reset" {
+                            self.active_warning = None;
+                            self.send_message(
+                                client,
+                                &m.channel_login,
+                                "okay! content/trigger warning has been cleared.",
+                            )
+                            .await?;
+                        } else {
+                            self.active_warning = Some(content.to_string());
+                            self.users_greeted.clear();
+                            self.send_message(client, &m.channel_login, &format!("okay! issued a content/trigger warning with the following reason: \"{}\"", content)).await?;
+                        }
+                    } else {
+                        self.say_user_requested_warning(client, &m.channel_login, &m.sender.name)
+                            .await?;
+                    }
+                    true
+                } else {
+                    self.greet_user(client, &m.channel_login, &m.sender.name)
+                        .await?;
+                    false
+                }
+            }
+            _ => false,
+        };
+
+        Ok(handled)
+    }
+}
