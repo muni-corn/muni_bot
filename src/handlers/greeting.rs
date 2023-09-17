02bd7ce05120a1e40297bbaaf8e6a033ed584117
municorn <municorn@musicaloft.com>
2023-09-17 17:01:26 -0500

    handlers/greeting: Add central `get_greeting_message` method
    that can be used between different handlers

--- src/handlers/greeting.rs
+++ src/handlers/greeting.rs
@@ -1,5 +1,6 @@
 use async_trait::async_trait;
 use lazy_static::lazy_static;
+use poise::serenity_prelude::{Context, Message};
 use rand::Rng;
 use regex::Regex;
 use twitch_irc::message::ServerMessage;
@@ -11,6 +12,38 @@ use crate::twitch::{
 
 pub struct GreetingHandler;
 
+lazy_static! {
+    static ref HI_REGEX: Regex =
+        Regex::new(r"(?i)(?:hi+|hey+|hello+|howdy|sup).*muni.*bot").unwrap();
+}
+
+impl GreetingHandler {
+    /// Returns a greeting message if applicable, or None if not to keep quiet.
+    fn get_greeting_message(user_name: &str, message_text: &str) -> Option<String> {
+        if message_text.trim().starts_with("uwu") {
+            if user_name == "Linokii" {
+                Some("linokii uwu<3".to_string())
+            } else {
+                None
+            }
+        } else if HI_REGEX.is_match(message_text) {
+            // send a hi message back
+            // pick a template
+            let template_index = rand::thread_rng().gen_range(0..HELLO_TEMPLATES.len());
+            let mut greeting = HELLO_TEMPLATES[template_index].replace("{name}", &user_name);
+
+            // if the message was sent from linokii, append a very special uwu
+            if user_name == "Linokii" {
+                greeting.push_str(" uwu");
+            }
+
+            Some(greeting)
+        } else {
+            None
+        }
+    }
+}
+
 #[async_trait]
 impl TwitchMessageHandler for GreetingHandler {
     async fn handle_twitch_message(
@@ -18,35 +51,10 @@ impl TwitchMessageHandler for GreetingHandler {
         client: &MuniBotTwitchIRCClient,
         message: ServerMessage,
     ) -> Result<bool, TwitchHandlerError> {
-        lazy_static! {
-            static ref HI_REGEX: Regex =
-                Regex::new(r"(?i)(?:hi+|hey+|hello+|howdy|sup).*muni.*bot").unwrap();
-        }
-
         let handled = if let ServerMessage::Privmsg(m) = message {
-            if m.message_text.trim().starts_with("uwu") {
-                if m.sender.name == "Linokii" {
-                    self.send_twitch_message(client, &m.channel_login, "linokii uwu<3")
-                        .await?;
-                    true
-                } else {
-                    false
-                }
-            } else if HI_REGEX.is_match(&m.message_text) {
-                // send a hi message back
-                // pick a template
-                let template_index = rand::thread_rng().gen_range(0..HELLO_TEMPLATES.len());
-                let mut greeting =
-                    HELLO_TEMPLATES[template_index].replace("{name}", &m.sender.name);
-
-                // if the message was sent from linokii, append a very special uwu
-                if m.sender.name == "Linokii" {
-                    greeting.push_str(" uwu");
-                }
-
-                self.send_twitch_message(client, &m.channel_login, &greeting)
+            if let Some(response) = Self::get_greeting_message(&m.sender.name, &m.message_text) {
+                self.send_message(client, &m.channel_login, &response)
                     .await?;
-
                 true
             } else {
                 false
