fd1bd1ae8cfa3fc49c7af03f8863cd63db3cf5d5
municorn <municorn@musicaloft.com>
2023-10-01 23:33:50 -0500

    feat(handlers): add `TwitchHandlerCollection` and `DiscordHandlerCollection`

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,6 +1,15 @@
+use std::sync::Arc;
+
+use tokio::sync::Mutex;
+
+use crate::{discord::handler::DiscordMessageHandler, twitch::handler::TwitchMessageHandler};
+
 pub mod bonk;
 pub mod content_warning;
 pub mod greeting;
 pub mod lurk;
 pub mod raid_msg;
 pub mod socials;
+
+pub type TwitchHandlerCollection = Vec<Arc<Mutex<dyn TwitchMessageHandler>>>;
+pub type DiscordHandlerCollection = Vec<Arc<Mutex<dyn DiscordMessageHandler>>>;
