e2434f019451349e0bdbd141d8ba9774748d3fba
municorn <municorn@musicaloft.com>
2023-12-21 10:49:50 -0600

    feat(twitch): add magical handler

--- src/twitch/bot.rs
+++ src/twitch/bot.rs
@@ -8,7 +8,7 @@ use twitch_irc::{
 use crate::handlers::{
     affection::AffectionHandler, bonk::BonkHandler, greeting::GreetingHandler, lift::LiftHandler,
     lurk::LurkHandler, quotes::QuotesHandler, raid_msg::RaidMsgHandler, shoutout::ShoutoutHandler,
-    socials::SocialsHandler,
+    socials::SocialsHandler, magical::MagicalHandler,
 };
 
 use super::{
@@ -36,6 +36,7 @@ impl TwitchBot {
                 Box::new(LiftHandler::new()),
                 Box::new(ShoutoutHandler),
                 Box::new(AffectionHandler),
+                Box::new(MagicalHandler),
             ],
         }
     }
