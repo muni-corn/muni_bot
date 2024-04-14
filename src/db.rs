ee4d4844bbcdfcca2c4013772deb952d087b162f
municorn <municorn@musicaloft.com>
2024-04-13 22:51:05 -0600

    feat(db): add `as_thing`

--- src/db.rs
+++ src/db.rs
@@ -12,18 +12,20 @@ pub trait DbItem<C: Connection>: Serialize + DeserializeOwned {
 
     fn get_id(&self) -> Id;
 
+    fn as_thing(&self) -> Thing {
+        Thing {
+            id: self.get_id().clone(),
+            tb: Self::NAME.to_string(),
+        }
+    }
+
     async fn get_from_db(
         db: &Surreal<C>,
         query: Self::GetQuery,
     ) -> Result<Option<Self>, surrealdb::Error>;
 
     async fn update_in_db(&self, db: &Surreal<C>) -> Result<Option<Self>, surrealdb::Error> {
-        db.update(Thing {
-            id: self.get_id().clone(),
-            tb: Self::NAME.to_string(),
-        })
-        .content(self)
-        .await
+        db.update(self.as_thing()).content(self).await
     }
 
     async fn delete_from_db(&self, db: &Surreal<C>) -> Result<Option<Self>, surrealdb::Error> {
