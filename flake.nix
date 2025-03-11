dfbbbb2a190f5c35e540babf7c45d7f25bc7a172
municorn <municorn@musicaloft.com>
2025-03-11 11:26:33 -0600

    chore: format flake and add cargo-release

--- flake.nix
+++ flake.nix
@@ -8,110 +8,126 @@
     crane.url = "github:ipetkov/crane";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    crane,
-    fenix,
-    utils,
-  }:
-    utils.lib.eachDefaultSystem
-    (system: let
-      name = "muni_bot";
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [fenix.overlays.default]; # for rust-analyzer-nightly
-      };
-      lib = pkgs.lib;
-
-      # make rust toolchain
-      toolchain = with fenix.packages.${system};
-        combine [
-          complete.rust-src
-          complete.rustc-codegen-cranelift-preview
-          default.cargo
-          default.clippy
-          default.rustfmt
-          rust-analyzer
-          targets.wasm32-unknown-unknown.latest.rust-std
-        ];
-
-      # make build library
-      craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
-
-      # build artifacts
-      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-
-      # establish commonly used arguments
-      commonArgs = {
-        src = lib.cleanSourceWith {
-          src = self;
-          filter = path: type:
-            (lib.hasInfix "/assets/" path)
-            || (lib.hasInfix "/style/" path)
-            || (lib.hasSuffix "tailwind.config.js" path)
-            || (craneLib.filterCargoSources path type);
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crane,
+      fenix,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        name = "muni_bot";
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ fenix.overlays.default ]; # for rust-analyzer-nightly
         };
-        strictDeps = true;
-        stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
+        lib = pkgs.lib;
 
-        inherit nativeBuildInputs buildInputs cargoArtifacts;
-      };
+        # make rust toolchain
+        toolchain =
+          with fenix.packages.${system};
+          combine [
+            complete.rust-src
+            complete.rustc-codegen-cranelift-preview
+            default.cargo
+            default.clippy
+            default.rustfmt
+            rust-analyzer
+            targets.wasm32-unknown-unknown.latest.rust-std
+          ];
 
-      nativeBuildInputs = with pkgs; [
-        clang
-        glibc
-        leptosfmt
-        pkg-config
-        trunk
-      ];
-      buildInputs = with pkgs; [libressl];
+        # make build library
+        craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
 
-      muni_bot = craneLib.buildPackage (commonArgs
-        // {
-          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-        });
-    in {
-      # `nix build`
-      packages.default = muni_bot;
+        # build artifacts
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-      # `nix run`
-      apps.default = utils.lib.mkApp {
-        name = name;
-        drv = self.packages."${system}".default;
-        exePath = "/bin/${name}";
-      };
-
-      # `nix flake check`
-      checks = {
-        inherit muni_bot;
-        clippy =
-          craneLib.cargoClippy (commonArgs
-            // {cargoClippyExtraArgs = "--all-targets --all-features";});
-      };
-
-      # `nix develop`
-      devShells.default = let
-        moldDevShell =
-          craneLib.devShell.override
-          {
-            mkShell = pkgs.mkShell.override {
-              stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
-            };
+        # establish commonly used arguments
+        commonArgs = {
+          src = lib.cleanSourceWith {
+            src = self;
+            filter =
+              path: type:
+              (lib.hasInfix "/assets/" path)
+              || (lib.hasInfix "/style/" path)
+              || (lib.hasSuffix "tailwind.config.js" path)
+              || (craneLib.filterCargoSources path type);
           };
-      in
-        moldDevShell {
-          checks = self.checks.${system};
+          strictDeps = true;
+          stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
 
-          packages = with pkgs; [leptosfmt cargo-watch cargo-outdated flyctl cargo-machete];
-
-          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-          RUST_LOG = "error,muni_bot=debug";
-          LEPTOS_TAILWIND_VERSION = "v3.4.14";
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+          inherit nativeBuildInputs buildInputs cargoArtifacts;
         };
-    })
+
+        nativeBuildInputs = with pkgs; [
+          clang
+          glibc
+          leptosfmt
+          pkg-config
+          trunk
+        ];
+        buildInputs = with pkgs; [ libressl ];
+
+        muni_bot = craneLib.buildPackage (
+          commonArgs
+          // {
+            LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+          }
+        );
+      in
+      {
+        # `nix build`
+        packages.default = muni_bot;
+
+        # `nix run`
+        apps.default = utils.lib.mkApp {
+          name = name;
+          drv = self.packages."${system}".default;
+          exePath = "/bin/${name}";
+        };
+
+        # `nix flake check`
+        checks = {
+          inherit muni_bot;
+          clippy = craneLib.cargoClippy (
+            commonArgs // { cargoClippyExtraArgs = "--all-targets --all-features"; }
+          );
+        };
+
+        # `nix develop`
+        devShells.default =
+          let
+            moldDevShell = craneLib.devShell.override {
+              mkShell = pkgs.mkShell.override {
+                stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
+              };
+            };
+          in
+          moldDevShell {
+            checks = self.checks.${system};
+
+            packages =
+              buildInputs
+              ++ (with pkgs; [
+                leptosfmt
+                cargo-release
+                cargo-watch
+                cargo-outdated
+                flyctl
+                cargo-machete
+              ]);
+
+            LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+            RUST_LOG = "error,muni_bot=debug";
+            LEPTOS_TAILWIND_VERSION = "v3.4.14";
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+          };
+      }
+    )
     // {
       overlays.default = final: prev: {
         muni_bot = self.packages.${prev.system}.default;
